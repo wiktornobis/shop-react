b62950021576b8014ec6c4d9b9b76520abfbf610
Wiktor Nobis <nobis171@wp.pl>
2022-05-10 13:51:00 +0200

    completed Swiper

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from "../hero/Hero";
 import Newsletter from "../newsletter/Newsletter";
+import Slider from "../slider/Slider";
 
 
 function MainPage(props) {
     return (
         <>
             <Hero />
+            <Slider />
             <Newsletter />
         </>
     );
