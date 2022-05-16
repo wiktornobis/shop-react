2a03cb71e1dcf22a7bd06b723351c13a9dda6771
Wiktor Nobis <nobis171@wp.pl>
2022-05-16 21:03:30 +0200

    first step config

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,28 +1,53 @@
-import React from 'react';
-import './_products.scss';
-import products_clothes from "../data/DataProducts";
+import React, {useEffect, useState} from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import {db} from "../../api/apiConfig";
 
+export default function ListClothes() {
+    const [clothes, setClothes] = useState([]);
 
-function Products() {
-    const listItems = products_clothes.map((item) =>
-        <div className="hoodie" key={item.id}>
-            <div className="hoodie__img">
-                <img src={item.image} alt={item.name} />
+    useEffect(() => {
+        getClothes()
+    }, [])
+
+    useEffect(() => {
+        console.log(clothes)
+
+    }, [clothes])
+
+    function getClothes() {
+        const clothesCollectionRef = collection(db, 'clothes')
+        getDocs(clothesCollectionRef)
+            .then(response => {
+                const list = response.docs.map(doc => ({
+                    data: doc.data(),
+                    id: doc.id,
+                    price: doc.data().price,
+                    image: doc.data().image,
+
+                }))
+                setClothes(list)
+
+            }).catch(error => console.log(error.message))
+
+    }
+
+    return (
+        <div>
+            <h4>List Products</h4>
+            <div>
+                {clothes.map(list => (
+                    <div key={list.id}>
+                        <h4>{list.image}</h4>
+                        <h2 >{list.data.name}</h2>
+                        <h3>{list.price}</h3>
+
+                    </div>
+
+                ))}
+
             </div>
-            <div className="hoodie__name">
-                <h2>{item.name}</h2>
-            </div>
-            <div className="hoodie__price">
-                <p>{item.price}</p>
-            </div>
-            <button className='hoodie__btn' >Add to cart</button>
+
         </div>
     );
-    return (
-        <div className="products">
-            {listItems}
-        </div>
-    )
 }
 
-export default Products;
