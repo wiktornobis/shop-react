3112ce9ca9dffb56448c412f3048bb963a408dff
Wiktor Nobis <nobis171@wp.pl>
2022-05-16 09:50:29 +0200

    products

---
+++ src/components/data/DataProducts.js
@@ -0,0 +1,44 @@
+
+
+const products_clothes =  [
+    {
+        id: '1',
+        name: 'Hoodie 1',
+        price: '239.00',
+        image: '../../../src/components/image/image1.jpg',
+    },
+    {
+        id: '2',
+        name: 'Hoodie 2',
+        price: '239.00',
+        image: 'image/image1.jpg',
+    },
+    {
+        id: '3',
+        name: 'Hoodie 3',
+        price: '239.00',
+        image: 'image/image1.jpg',
+    },
+    {
+        id: '4',
+        name: 'Hoodie 4',
+        price: '239.00',
+        image: 'image/image1.jpg',
+    },
+    {
+        id: '5',
+        name: 'Hoodie 5',
+        price: '239.00',
+        image: 'image/image1.jpg',
+    },
+    {
+        id: '6',
+        name: 'Hoodie 6',
+        price: '239.00',
+        image: 'image/image1.jpg',
+    },
+
+]
+
+
+export default products_clothes;
