280675459f935d5bfa686573b506023a6ced969d
Wiktor Nobis <nobis171@wp.pl>
2022-05-19 15:53:48 +0200

    completed contact-section_02

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -3,20 +3,20 @@ import './_aboutUs.scss';
 
 function AboutUs(props) {
     return (
-        <div className='all-contact'>
-            <h2 className='main-contact'>CONTACT</h2>
-            <div className='contact'>
-                <div className="contact__left">
-                    <h3 className='second-contact'>CUSTOMER SERVICE</h3>
-                    <div className="contact__left--text">
+        <div className='all-about-us'>
+            <h2 className='main-about'>ABOUT US</h2>
+            <div className='about'>
+                <div className="about__left">
+                    <h3 className='second-about'>CUSTOMER SERVICE</h3>
+                    <div className="about__left--text">
                         <p>E:mail: example123@.gmail.com</p>
                         <p>Full name: Example ® - Official Online Boutique</p>
                         <p>Address: 118 Kasprzaka Street, 01-234 Warsaw (POLAND)</p>
                     </div>
                 </div>
-                <div className="contact__right">
-                    <h3 className='second-contact'>STORE OWNER</h3>
-                    <div className="contact__right--text">
+                <div className="about__right">
+                    <h3 className='second-about'>STORE OWNER</h3>
+                    <div className="about__right--text">
                         <p>Official Online Boutique Sp. z o.o.</p>
                         <p>Kasprzaka 118, 01-234 Warsaw(POLAND)</p>
                         <p>NIP: XXXXXXXX</p>
